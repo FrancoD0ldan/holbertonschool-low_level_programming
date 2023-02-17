bb176edbd75a198ffaab838ff5a727e7c0f07fbf
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-17 07:49:32 -0800

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+
+	return (0);
+}
