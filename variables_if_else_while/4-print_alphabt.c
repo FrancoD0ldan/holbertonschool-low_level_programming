a08226ff771f9283fbc2f900a398211ad783f8b3
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-18 11:05:54 -0800

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * msin - start
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+{
+	if (x != 'e' && x != 'q')
+	putchar(x);
+}
+	putchar('\n');
+
+	return (0);
+}
