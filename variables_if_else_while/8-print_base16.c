c6bc4046f7ec31fa1d6efe89eb497c77546b0ff3
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-18 11:58:11 -0800

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 57; x++)
+	{
+	putchar(x);
+	}
+	for (y = 97; y <= 102; y++)
+
+	{
+	putchar (y);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
