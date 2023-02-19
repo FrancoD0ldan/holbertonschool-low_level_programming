b0ff365c66249ec92c9b5e3d6c15402d43ed8a9e
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-19 14:00:20 -0800

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: always 0
+ *
+ */
+void
+print_alphabet_x10(void)
+{
+	char x = 'a';
+	int y = 0;
+
+	for (y = 0; y <= 9; y++)
+	{
+		x = 'a';
+		while (x <= 'z')
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar('\n');
+	}
+}
