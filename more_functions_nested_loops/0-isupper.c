3f466a1acbfe334377cdcd75e9d545673f87d9d8
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-22 09:19:59 -0800

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,21 @@
 #include "main.h"
+/**
+ * mai - start
+ *@c: variable
+ * Return: always 0
+ *
+ */
 
 int
 _islower(int c)
+
 {
-        if (c >= 'a' && c <= 'z')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
