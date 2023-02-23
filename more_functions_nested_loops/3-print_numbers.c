a8ad50f70f799164fbeb3eb8b6132ee3000471c6
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-22 16:59:11 -0800

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_putchar - check the code
+ *
+ * Return: Always 0.
+ */
+void
+print_numbers(void)
+{
+	int s;
+
+	for (s = 48; s <= 57; s++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
