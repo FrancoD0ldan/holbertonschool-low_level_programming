6598d8bb0ded63655826263d619352f2d8005423
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-23 09:55:48 -0800

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print-line - check the code
+ *Return:Always 0.
+ *@n: varibale
+ */
+
+void
+print_line(int n)
+{
+	int num
+
+	for (num = 0; num < n; num++)
+	{
+		_putchar(95)
+	}
+		_putchar('\n'),
+}
