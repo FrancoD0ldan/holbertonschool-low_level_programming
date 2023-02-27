6f15222fd4f70a0e4b6354359d640399ecc30b80
FrancoD0ldan <6309@holbertonstudents.com>
2023-02-27 09:35:19 -0800

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * @n: variable
+ *
+ * Return: Always 0
+ *
  */
 
 void
-rest_to_98(int *n)
+reset_to_98(int *n)
 
 {
 	*n = 98;
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,5 +19,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
-
-
